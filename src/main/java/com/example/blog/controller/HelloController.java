dc1e94b7b51f978fd6e9371904aa63685bf33d49
ccziwe <ccziwe@qq.com>
2020-03-01 20:52:41 +0800

    war包改成jar包，增加页面静态文件

--- src/main/java/com/example/blog/controller/HelloController.java
+++ src/main/java/com/example/blog/controller/HelloController.java
@@ -1,16 +1,13 @@
 package com.example.blog.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("hello")
-    @ResponseBody
+    @RequestMapping("index")
     public String hello(){
-        return "hello ccziwe";
+        return "index.html";
     }
 }
